a363314a774c82c09688adab36d01c485cf15b23
Manichand <mani.chand01@gmail.com>
2022-08-20 09:03:40 +0530

    complete realese

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
 //import logo from './logo.svg';
 //import './App.css';
+import uuid from 'react-uuid'
+import { useState,useEffect,useRef } from 'react';
 import './components/Notes.js'   
 //import ReactDOM from 'react-dom';
 import Notes from './components/Notes.js';
 //import { Route, BrowserRouter as Router,Routes } from  'react-router-dom'  
 
 function App() {
+  const inputRef = useRef()
+
+  const getLocalItmes=()=>{
+    let list=localStorage.getItem('item')
+    console.log(list)
+    if(list){
+        return JSON.parse(localStorage.getItem('item'))
+    }
+    else{
+      return []
+    }
+}
+  const [note,setNote]=useState(getLocalItmes())
+  /*
+  const [name,setName]=useState(" ")
+  onst [users,setUser]=useState([])
+  const addUser = () =>{
+   
+  }
+  const nam = () =>{
+    const hai=document.querySelector('.lable-mail').value   
+    setName(hai)
+   }*/
+    const add=()=>{
+        var n=document.querySelector('.in-txt').value
+         setNote([...note,{
+            value:n,
+            id:uuid()
+         }])
+         inputRef.current.focus()
+    }
+    const remove=(id) =>{
+        console.log(id)
+        var heloo=note.filter(function(notes){ 
+            return notes.id !== id})
+        console.log(heloo)
+        setNote(heloo)
+    }
+    useEffect(() =>{
+         localStorage.setItem('item',JSON.stringify(note))
+      },[note])
+
+      const handleKeypress = e => {
+        //it triggers by pressing the enter key
+      if (e.key === "Enter") {
+        add()
+        inputRef.current.focus()
+      }
+      };
   return (
         <div>
-          <Notes/>
+          <Notes add={add} remove={remove} note={note} handleKeypress={handleKeypress} inputRef={inputRef}/>
         </div> 
       
   );
 }
+
 
 export default App;
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,51 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { AiOutlineDelete } from "react-icons/ai";
+//import { useState } from "react";
+import { BiBookAdd } from "react-icons/bi";
+
 import '../App.css';
-const Notes = () => {
-    const [note,setnote]=useState([ ])
-    const add = (e) =>{
-        var hai=document.querySelector(".textField").value
-        setnote([...note,{
-            value: hai,
-            id:note.length
-        }])
-    }
-    const remove=(id) =>{
-        console.log(id)
-        var heloo=note.filter(function(notes){ 
-            return notes.id !== id})
-        console.log(heloo)
-        setnote(heloo)
-    }
-
+const Notes = ({add,remove,note,handleKeypress,inputRef}) => { 
     return ( 
-      <div>
-        <div className="container">
-            <div className="formField">
-                <header>
-                    <h1>Notes</h1>
-                </header>
-                <div>
-                    <input type="text"  placeholder="Enter a Note"  className="textField" autoFocus />
-                    <button onClick={add}  className="btn1">Submit</button>
-                </div>
-                
-                
-
-                    <div className="container-2">
-        <div className="listField">
-                <ul>
-                {note.map((notes) =><li  key={notes.id}> {notes.value} 
-                <br/>
-                <button onClick={() => remove(notes.id)}>Delete</button>
-                </li>)} 
-                </ul> 
-            </div>
-                
-            </div>
-            </div> 
-            </div>
+        <div>
+        <div className="title">
+        <h1>Notes App</h1>
+        </div>
+        <div className="inputarea">
+            <input type="text" 
+             onKeyPress={handleKeypress} 
+            placeholder="enter any thing"  className="in-txt" />
+            <button className="add" onClick={add}><BiBookAdd size={35} className="icon-add"/></button>
+        </div>
+        <span className="otr">
+        <div className="notesarea">
+        {
+            note.map( notes => 
+             <td className="tble">
+                <tr key={notes.id} className="val"> {notes.value} </tr>
+                <button onClick={()=> remove(notes.id)} className="btn-del"><AiOutlineDelete size={30} className="icon-del"/></button>
+            </td> )
+        }
         </div>
         
+        </span>
+    </div>      
      );
 }
  
